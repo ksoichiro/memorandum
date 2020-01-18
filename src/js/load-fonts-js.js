70db491812d8c87f0241d41e3fb43c9a6dbd2f43
Soichiro Kashima <soichiro.kashima@gmail.com>
2020-01-18 19:44:10 +0900

    Externalize font loading JavaScript

---
+++ src/js/load-fonts-js.js
@@ -0,0 +1,7 @@
+(function (d, f) {
+  var l = d.createElement('link');
+  l.rel = 'stylesheet';
+  l.href = f;
+  var s = d.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(l, s);
+})(document, 'https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700&display=swap');
